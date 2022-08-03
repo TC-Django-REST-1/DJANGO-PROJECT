6484132338e61db5f65b8fa235c365a42fb78184
Asraralqarnii <asraralqarni1996@hotmail.com>
2022-08-03 20:43:49 +0300

    employees done

--- CCMS/organization/urls.py
+++ CCMS/organization/urls.py
@@ -4,10 +4,18 @@ from . import views
 app_name = "organization" 
 
 urlpatterns = [
-    #Service
+    #Services
     path("add/service", views.new_service, name="new_service"),
     path("all/services", views.read_service, name="read_service"),
     path("update/service/<service_id>", views.update_service, name="update_service"),
     path("delete/service/<service_id>", views.delete_service, name="delete_service"),
+
+    #Employees
+    path("add/employee", views.new_employee, name="new_employee"),
+    path("all/employees", views.read_employee, name="read_employee"),
+    path("delete/employee/<employee_id>", views.delete_employee, name="delete_employee"),
+
+    #Companies
+
     
 ]
--- CCMS/organization/views.py
+++ CCMS/organization/views.py
@@ -4,10 +4,12 @@ from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework import status
 
-from .models import Service
+from .models import Service, Employee
 from .serializers import AdminstrationSerializer,EmployeeSerializer,ServiceSerializer,RequsetSerializer,TaskSerializer
 
-# Create your views here.
+
+#           ******************************** Services ********************************
+
 # Add new service 
 @api_view(['POST'])
 def new_service(request:Request):
@@ -56,12 +58,55 @@ def update_service(request:Request, service_id):
 
 # Delete service by id 
 @api_view(['DELETE'])
-def delete_service(request:Request, brand_id):
+def delete_service(request:Request, service_id):
     
-    service = Service.objects.get(id = brand_id)
+    service = Service.objects.get(id = service_id)
     service.delete()
 
     response_data = {
         "Message" : "Service information deleted!",
     }
     return Response(response_data, status=status.HTTP_200_OK)
+
+
+#           ******************************** Employee ********************************
+
+# Add new Employee 
+@api_view(['POST'])
+def new_employee(request:Request):
+
+    employee_serializer = EmployeeSerializer(data=request.data)
+
+    if employee_serializer.is_valid():
+        employee_serializer.save()
+    else:
+        return Response({"Message" : "Couldn't Add Employee", "errors" : employee_serializer.errors}, status=status.HTTP_403_FORBIDDEN)
+
+    return Response({"Message" : "Employee added successfully"}, status=status.HTTP_201_CREATED)
+
+# Get all Employees
+@api_view(['GET'])
+def read_employee(request:Request):
+
+    all_employees = Employee.objects.all()
+    employees_info = EmployeeSerializer(instance=all_employees, many=True).data
+
+    response_data = {
+        "Message" : "All Employees List",
+        "Brand" : employees_info
+    }
+
+    return Response(response_data, status=status.HTTP_200_OK)
+
+# Delete employee by id 
+@api_view(['DELETE'])
+def delete_employee(request:Request, employee_id):
+    
+    service = Employee.objects.get(id = employee_id)
+    service.delete()
+
+    response_data = {
+        "Message" : "Employee information deleted!",
+    }
+    return Response(response_data, status=status.HTTP_200_OK)
+    
