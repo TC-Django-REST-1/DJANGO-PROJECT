d92ad06d1324e6e6e5a57c4f7d1e7806199384fc
saud <s.s.alghamdi@hotmail.com>
2022-08-03 17:17:30 +0300

    add some models

--- Vehicles/cars/models.py
+++ Vehicles/cars/models.py
@@ -1,30 +1,77 @@
 from django.db import models
 
+
+
+# This model is to save all information about brand
 class Brand(models.Model):
-    brand_name = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255)
     description = models.TextField()
     established_in = models.PositiveIntegerField()
-    Origin = models.CharField(max_length=255)
-    Founder = models.CharField(max_length=255)
-    Headquarters = models.TextField()
-    # Products_classes = models.ManyToManyField(ProductCategory, blank=True, null=True, blank=True, related)
-    # Products_categories = models.ManyToManyField(ProductCategory, blank=True, null=True, blank=True, related)
+    origin = models.CharField(max_length=255)
+    founder = models.CharField(max_length=255)
+    headquarters = models.TextField()
+    last_revenue = models.DecimalField(max_digits=12, decimal_places=2)
+    year = models.PositiveIntegerField()
+    remarks = models.TextField()
 
-class Classes(models.Model):
-    pass
+    def __str__(self):
+        return self.brand
 
+
+# This model is to track all modifications that take a place in any brand record, answring these questions (when? who? did what?)
+class BrandHistory(models.Model):
+    brand = models.CharField(max_length=255)
+    description = models.TextField()
+    established_in = models.PositiveIntegerField()
+    origin = models.CharField(max_length=255)
+    founder = models.CharField(max_length=255)
+    headquarters = models.TextField()
+    last_revenue = models.DecimalField(max_digits=12, decimal_places=2)
+    year = models.PositiveIntegerField()
+    remarks = models.TextField()
+    modified_by = models.CharField(max_length=255)
+    modification_date = models.DateField()
+
+    def __str__(self):
+        return self.brand
+
+# This model is to list the general classes that can be used to categorize any brand class 
+class GeneralClasses(models.Model):
+    general_class = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.general_class
+
+
+# This model is to save all information about classes in each brand like, Luxury cars ,Commercial cars, sport cars,
+class BrandsClasses(models.Model):
+    class_name = models.CharField(max_length=255)
+    class_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    general_class =  models.ForeignKey(GeneralClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
+    targeted_customers = models.CharField(max_length=255)
+    quality = models.CharField(max_length=255)
+    exclusive = models.BooleanField(default=False)
+    remarks = models.TextField()
+
+    def __str__(self):
+        return self.class_name + " | " + self.class_brand.brand + " | " + self.general_class
+    
+
+# This model is to save categories of all brands, Ex, sedan car, Family car, 4x4 car, industiral car ... etc.
 class Categories(models.Model):
-    pass
+    category_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    category_name = models.CharField(max_length=255)
+    targeted_customers = models.CharField(max_length=255)
+    targeted_cost_range = models.CharField(max_length=255)
+    Limited = models.BooleanField(default=False)
+    remarks = models.TextField()
 
     
 class Car(models.Model):
-    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    car_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    clas = models.ForeignKey(Classes, on_delete=models.CASCADE)
-    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    car_class = models.ForeignKey(BrandsClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
+    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default='Need to be added manually')
     image_url = models.URLField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     ongoing_product = models.BooleanField(default=False)
-
-
-
