d82fab215fba8567549ae4da9df72189892507a0
saud <s.s.alghamdi@hotmail.com>
2022-08-03 18:03:22 +0300

    register all models as start point, later add or remove some

--- Vehicles/cars/admin.py
+++ Vehicles/cars/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Brand,Car,BrandHistory,GeneralClasses,BrandsClasses,Categories
 
 # Register your models here.
+admin.site.register(Brand)
+admin.site.register(Car)
+admin.site.register(BrandHistory)
+admin.site.register(GeneralClasses)
+admin.site.register(BrandsClasses)
+admin.site.register(Categories)
+
