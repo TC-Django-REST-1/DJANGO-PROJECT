96d60858db9e397a30a5b469be4e2362ae58e870
saud <s.s.alghamdi@hotmail.com>
2022-08-05 02:17:53 +0300

    commit db changes

--- Vehicles/cars/models.py
+++ Vehicles/cars/models.py
@@ -46,40 +46,40 @@ class GeneralClasses(models.Model):
         return self.general_class
 
 
-# This model is to save all information about classes in each brand like, Luxury cars ,Commercial cars, sport cars,
-class BrandsClasses(models.Model):
-    class_name = models.CharField(max_length=255)
-    class_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    general_class =  models.ForeignKey(GeneralClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
-    targeted_customers = models.CharField(max_length=255)
-    quality = models.CharField(max_length=255)
-    exclusive = models.BooleanField(default=False)
-    remarks = models.TextField()
+# # This model is to save all information about classes in each brand like, Luxury cars ,Commercial cars, sport cars,
+# class BrandsClasses(models.Model):
+#     class_name = models.CharField(max_length=255)
+#     class_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+#     general_class =  models.ForeignKey(GeneralClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
+#     targeted_customers = models.CharField(max_length=255)
+#     quality = models.CharField(max_length=255)
+#     exclusive = models.BooleanField(default=False)
+#     remarks = models.TextField()
 
-    def __str__(self):
-        return self.class_name + " | " + self.class_brand.brand + " | " + self.general_class
+#     def __str__(self):
+#         return self.class_name + " | " + self.class_brand.brand + " | " + self.general_class
     
 
-# This model is to save categories of all brands, Ex, sedan car, Family car, 4x4 car, Truks, industiral cars ... etc.
-class Categories(models.Model):
-    category_name = models.CharField(max_length=255)
-    targeted_customers = models.CharField(max_length=255)
-    targeted_cost_range = models.CharField(max_length=255)
-    Limited = models.BooleanField(default=False)
-    remarks = models.TextField()
+# # This model is to save categories of all brands, Ex, sedan car, Family car, 4x4 car, Truks, industiral cars ... etc.
+# class Categories(models.Model):
+#     category_name = models.CharField(max_length=255)
+#     targeted_customers = models.CharField(max_length=255)
+#     targeted_cost_range = models.CharField(max_length=255)
+#     Limited = models.BooleanField(default=False)
+#     remarks = models.TextField()
 
-    def __str__(self):
-        return self.category_name
+#     def __str__(self):
+#         return self.category_name
 
     
-class Car(models.Model):
-    car_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    car_class = models.ForeignKey(BrandsClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
-    category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default='Need to be added manually')
-    image_url = models.URLField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    ongoing_product = models.BooleanField(default=False)
+# class Car(models.Model):
+#     car_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     car_class = models.ForeignKey(BrandsClasses, on_delete=models.SET_DEFAULT, default='Need to be added manually')
+#     category = models.ForeignKey(Categories, on_delete=models.SET_DEFAULT, default='Need to be added manually')
+#     image_url = models.URLField(blank=True)
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     ongoing_product = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
--- Vehicles/cars/urls.py
+++ Vehicles/cars/urls.py
@@ -18,23 +18,23 @@ urlpatterns = [
     path('remove_general_class/<general_class_id>', views.remove_general_class, name='remove_general_class'),
 
 
-    path('add_Brands_class/', views.add_Brands_class, name='add_Brands_class'),
-    path('Brands_class_list/', views.Brands_class_list, name='Brands_class_list'),
-    path('update_Brands_class/<Brand_class_id>', views.update_Brands_class, name='update_Brands_class'),
-    path('remove_Brands_class/<Brand_class_id>', views.remove_Brands_class, name='remove_Brands_class'),
+    # path('add_Brands_class/', views.add_Brands_class, name='add_Brands_class'),
+    # path('Brands_class_list/', views.Brands_class_list, name='Brands_class_list'),
+    # path('update_Brands_class/<Brand_class_id>', views.update_Brands_class, name='update_Brands_class'),
+    # path('remove_Brands_class/<Brand_class_id>', views.remove_Brands_class, name='remove_Brands_class'),
 
     
-    path('add_new_Category/', views.add_new_Category, name='add_new_Category'),
-    path('Categories_list/', views.Categories_list, name='Categories_list'),
-    path('update_Category/<Brand_class_id>', views.update_Category, name='update_Category'),
-    path('remove_Category/<Brand_class_id>', views.remove_Category, name='remove_Category'),    
+    # path('add_new_Category/', views.add_new_Category, name='add_new_Category'),
+    # path('Categories_list/', views.Categories_list, name='Categories_list'),
+    # path('update_Category/<Brand_class_id>', views.update_Category, name='update_Category'),
+    # path('remove_Category/<Brand_class_id>', views.remove_Category, name='remove_Category'),    
         
     
-    path('add_new_car/', views.add_car, name='add_new_car'),
-    path('cars_list/', views.cars_list, name='cars_list'),
-    path('update_car/<car_id>', views.update_car, name='update_car'),
-    path('remove_car/<car_id>', views.remove_car, name='remove_car'),
+    # path('add_new_car/', views.add_car, name='add_new_car'),
+    # path('cars_list/', views.cars_list, name='cars_list'),
+    # path('update_car/<car_id>', views.update_car, name='update_car'),
+    # path('remove_car/<car_id>', views.remove_car, name='remove_car'),
 
 
-    path('brand_cars/<brand_title>', views.brand_cars_list, name='brand_cars_list'),
+    # path('brand_cars/<brand_title>', views.brand_cars_list, name='brand_cars_list'),
 ]
