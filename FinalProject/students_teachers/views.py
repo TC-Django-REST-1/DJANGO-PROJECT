651021dd9160450428f43422c6959a33d83a0f83
unknown <gngs11111@gmail.com>
2022-08-04 13:03:26 +0300

    created CRUD opreations

--- FinalProject/students_teachers/views.py
+++ FinalProject/students_teachers/views.py
@@ -16,6 +16,47 @@ students_list = [
 def display(request : Request):
     response_data = {
         "msg": "list of students",
-        "movies" : students_list
+        "students" : students_list
     }
     return Response(students_list)
+
+
+@api_view(["POST"])
+def add_student(request : Request):
+    #authenticated user info is stored in request.user
+    user = request.user
+
+    name = request.data["name"]
+    new_student = students_list(name=name)
+    new_student.save()
+
+    res_data = {
+        "msg" : "student add Successfully"
+    }
+
+    return Response(res_data)
+
+@api_view(['PUT'])
+def update_students(request : Request, students_list):
+
+    name = request.data["name"]
+    
+
+    student = students_list.objects.get(id=student)
+
+    student.title = name
+    student.save()
+
+    return Response({"msg" : "student is updated !"})
+
+
+@api_view(["DELETE"])
+def delete_student(request : Request, student_id):
+
+    try:
+        student = students_list.objects.get(id=student)
+        student.delete()
+    except Exception as e:
+        return Response({"msg" : "The student is not Found!"})
+
+    return Response({"msg" : f"delete the following student {student.name}"})
