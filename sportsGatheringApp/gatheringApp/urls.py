611da9a1b8a50203a05fb2cff08b171948ec9446
Nawaf Sh <nawaf.sheddi@gmail.com>
2022-08-04 11:20:20 +0300

    fix the sort end point

--- sportsGatheringApp/gatheringApp/urls.py
+++ sportsGatheringApp/gatheringApp/urls.py
@@ -10,4 +10,6 @@ path('add/', view=views.add_gather, name='add_gather'),
 path('update/<gather_id>', view=views.update_gather, name='Update_gather'),
 path('delete/<gather_id>', view=views.delete_gather, name="delete_gather"),
 path('join/<gather_id>', view=views.join_gather, name="join_gather"),
+path('sort/', view=views.sort_by, name="sort_gathers"),
+
 ]
